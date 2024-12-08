bfb354201fb01d2f590f69ad3b4951d93757f31d
Степанов Алексей <bssClient@gmail.com>
2024-12-08 15:39:44 +0500

    поправил описание

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  wb7MqqtAddr: '192.168.100.44', //'10.147.17.223',
+  //wb7MqqtAddr: '192.168.100.44',
+  wb7MqqtAddr: '10.147.17.223',
   wb6MqqtAddr: '10.147.17.184',
 };
