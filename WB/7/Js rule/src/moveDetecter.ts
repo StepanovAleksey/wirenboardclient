b3f55909d36bad2ee0ac2834a83319e32d28a162
Степанов Алексей <bssclient@gmail.com>
2024-12-24 13:46:01 +0500

    поправил алгоритм

--- WB/7/Js rule/src/moveDetecter.ts
+++ WB/7/Js rule/src/moveDetecter.ts
@@ -1,37 +1,46 @@
-/** Нижний порог срабатывания датчика */
-const LOW_LEVEL_ACC = 22;
-/** время на задержку выключения */
-const MOVE_DETECTION_DEBOUNCE_TIME_MS = 120_000;
-
 /**
  * @param moveDetectionController сигнал датчика движения
  * @param qController управляемый выход
+ * @param lowLvl Нижний порог срабатывания датчика
+ * @param debounceTimeSec время на задержку выключения, сек
  * */
 function cretaeMoveDetectionRules(
   moveDetectionController: string,
-  qController: string
+  qController: string,
+  lowLvl: number,
+  debounceTimeSec: number
 ) {
   let timeoutId: number = null;
+  function clearLastTimeout() {
+    if (!!timeoutId) {
+      clearTimeout(timeoutId);
+    }
+  }
   defineRule(`cretaeMoveDetection${moveDetectionController}_up`, {
     asSoonAs: function () {
-      return dev[moveDetectionController] >= LOW_LEVEL_ACC;
+      return dev[moveDetectionController] >= lowLvl;
     },
     then: function (newValue: number) {
-      dev[qController] = 1;
-      clearTimeout(timeoutId);
+      dev[qController] = true;
+      clearLastTimeout();
     },
   });
   defineRule(`cretaeMoveDetection${moveDetectionController}_down`, {
     asSoonAs: function () {
-      return dev[moveDetectionController] < LOW_LEVEL_ACC;
+      return dev[moveDetectionController] < lowLvl;
     },
     then: function (newValue: number) {
-      clearTimeout(timeoutId);
+      clearLastTimeout();
       timeoutId = setTimeout(() => {
-        dev[qController] = 0;
+        dev[qController] = false;
         timeoutId = null;
-      }, MOVE_DETECTION_DEBOUNCE_TIME_MS);
+      }, debounceTimeSec * 1000);
     },
   });
 }
-cretaeMoveDetectionRules("wb-msw-v4_91/Current Motion", "wb-mr6c_54/K4");
+cretaeMoveDetectionRules(
+  "wb-msw-v4_91/Current Motion",
+  "wb-mr6c_54/K4",
+  22,
+  60
+);
